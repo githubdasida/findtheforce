d3fda1ceb8951ead1cd3c849e8f15b4bf6146c87
githubdasida <josemarymontiel@gmail.com>
2019-12-28 19:55:19 +0100

    Peliculas 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,69 +11,16 @@
             <h1 class="glitch">Find<br>the Force</h1>
             <div class="botones d-flex">
                 <a href="{{ route('register') }}" class="btn btn-outline-light ml-0">Personajes Favoritos</a>
-                {{$r->count}}
             </div>
         </div>
         <div class="col-md">
-            <div class="card">
+            <div class="card card-long">
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row d-flex flex-column">
-                            <label for="email" class="col col-form-label">{{ __('E-Mail Address') }}</label>
-                            <div class="col">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row d-flex flex-column">
-                            <label for="password" class="col col-form-label">{{ __('Password') }}</label>
-
-                            <div class="col">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row d-flex">
-                            <div class="col-md">
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link p-0 mb-3" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
-                            <div class="col-md">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8">
-                                <button type="submit" class="btn btn-dark">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <ul>
+                        @foreach ($r['results'] as $film)
+                            <li><a href="http://">{{$flim->title}}</a></li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
