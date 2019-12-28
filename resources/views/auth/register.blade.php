ba945d53d037213874da6d37bf1a9fb5c99b97bf
githubdasida <josemarymontiel@gmail.com>
2019-12-28 12:29:19 +0100

    Implementación recaptcha
    En el controlador de registro, y sustitución color botón submit formulario de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -79,7 +79,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-dark">
                                     {{ __('Register') }}
                                 </button>
                             </div>
